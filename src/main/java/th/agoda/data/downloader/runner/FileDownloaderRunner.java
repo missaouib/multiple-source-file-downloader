75df6352270790a66b5d3834a5445316d44d5aa7
renuka.prasad <renuka.prasad@rakuten.com>
2017-04-28 19:41:28 +0900

    Added logging feature.

--- src/main/java/th/agoda/data/downloader/runner/FileDownloaderRunner.java
+++ src/main/java/th/agoda/data/downloader/runner/FileDownloaderRunner.java
@@ -1,6 +1,7 @@
 package th.agoda.data.downloader.runner;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import th.agoda.data.downloader.beans.UrlBean;
@@ -8,6 +9,7 @@ import th.agoda.data.downloader.common.FileDownloaderFactory;
 import th.agoda.data.downloader.input.InputUrlFileReader;
 
 @Component
+@Slf4j
 public class FileDownloaderRunner implements CommandLineRunner {
 
 	private InputUrlFileReader inputUrlFileReader;
@@ -24,6 +26,7 @@ public class FileDownloaderRunner implements CommandLineRunner {
 		List<UrlBean> urlBeans = inputUrlFileReader.getUrlBeans("/inputUrlList.txt");
 
 		for (UrlBean urlBean:urlBeans) {
+			log.debug("Downloading the file from urlBean {} ", urlBean);
 			fileDownloaderFactory.get(urlBean.getProtocol()).readFile(urlBean);
 		}
 	}
